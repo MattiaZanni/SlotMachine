fd25c6f54c354f11c7916e38acf9d2d9c794150b
Mattia Zanni <127590037+MattiaZanni@users.noreply.github.com>
2023-12-20 09:59:56 +0100

    Add files via upload

---
+++ Zanni.Mattia.4i.SlotMachine/Zanni.Mattia.4i.CLISlotMachine/Program.cs
@@ -0,0 +1,14 @@
+﻿using Zanni.Mattia._4i.LibSlotMachine;
+
+namespace Zanni.Mattia._4i.CLISlotMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, SlotMachine!");
+
+            SlotMachine S = new SlotMachine(10);
+        }
+    }
+}
---
+++ Zanni.Mattia.4i.SlotMachine/Zanni.Mattia.4i.LibSlotMachine/SlotMachine.cs
@@ -0,0 +1,15 @@
+﻿namespace Zanni.Mattia._4i.LibSlotMachine
+{
+    public class SlotMachine
+    {
+        public int Credito { get;set; }
+        public SlotMachine()
+        {
+            Credito = 0;
+        }
+        public SlotMachine(int credito)
+        {
+            Credito = credito;
+        }
+    }
+}
